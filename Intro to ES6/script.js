5ccdde8d2e93ede04e376d1072aa604ac8297f91
Suryansh <surya.icse@gmail.com>
2020-10-31 20:01:02 +0530

    Commited after final coding challenge on ECMA 6 , dated 31 Oct 2020

--- Intro to ES6/script.js
+++ Intro to ES6/script.js
@@ -17,14 +17,14 @@
 
 // // ES5
 // function driversLicence5(passedTest) {
-    
+
 //     if (passedTest) {
 //         console.log(firstName);
 //         var firstName = 'John';
 //         var yearOfBirth = 1990;
 //     }
-    
-    
+
+
 //     console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
 // }
 
@@ -32,15 +32,15 @@
 
 // // ES6
 // function driversLicence6(passedTest) {
-    
+
 //     //console.log(firstName);
 //     let firstName;
 //     const yearOfBirth = 1990;
-    
+
 //     if (passedTest) {
 //         firstName = 'John';
 //     }
-    
+
 //     console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
 // }
 
@@ -342,5 +342,257 @@
 // var emily = new Smith('emily',1983,'Diaz');
 
 ////////////////////////////////////////////////////
-// Lecture : Maps
+// Lecture : Maps(its a new key-value data struture in ES6)
 
+// const question = new Map();
+
+// question.set('question','What is official name of major JS version ?');
+// question.set(1,'ES5');
+// question.set(2,'ES5');
+// question.set(3,'ES 2015');
+// question.set(4,'ES7');
+// question.set('correct',3);
+// question.set(true,'Correct Answer');
+// question.set(false,'Please Try Again');
+
+// console.log(question.get('question')); 
+// // console.log(question.size); 
+
+// if(question.has(4)){
+//     // question.delete(4);
+//     // console.log('Answer 4 is here');
+// }
+
+// //delete full entry in a map
+// // question.clear();
+
+// //loop through a MAP
+
+// // question.forEach((key,value)=>console.log(`This is ${key} asigned to ${value}`));
+
+
+// for(let [key,value] of question.entries()){
+//     if(typeof(key)==='number'){
+//         console.log(`${key} : ${value}`);
+//     }
+// }
+
+// const ans = parseInt(prompt('Write correct Answer'));
+// console.log(question.get(ans === question.get('correct')));
+
+////////////////////////////////////////////////////
+// Lecture : Classes
+
+//In ES5 they are called as Function Constructor
+// var Person5 = function (name, yearOfBirth, job) {
+//     this.name = name;
+//     this.yearOfBirth = yearOfBirth;
+//     this.job = job;
+// }
+
+// Person5.prototype.calculateAge = function () {
+//     var age = new Date().getFullYear() - this.yearOfBirth;
+//     console.log(age);
+// }
+
+// var john5 = new Person5('John', 1990, 'teacher');
+
+// //ES6 Classes 
+
+// class Person6 { //all classes must have a constructor
+//     constructor(name, yearOfBirth, job) {
+//         this.name = name;
+//         this.yearOfBirth = yearOfBirth;
+//         this.job = job;
+//     }
+//     calculateAge() {
+//         var age = new Date().getFullYear() - this.yearOfBirth;
+//         console.log(age);
+//     }
+
+//     static greeting(){
+//         console.log('Hey there');
+//     }
+// }
+
+// const john6 = new Person6('John', 1990, 'teacher');
+// Person6.greeting();
+// john6.calculateAge();
+
+
+////////////////////////////////////////////////////
+// Lecture : Classes with Subclasses(similiar to inheritence in ES5)
+
+//In ES5 they are called as Function Constructor
+// var Person5 = function (name, yearOfBirth, job) {
+//     this.name = name;
+//     this.yearOfBirth = yearOfBirth;
+//     this.job = job;
+// }
+
+// Person5.prototype.calculateAge = function () {
+//     var age = new Date().getFullYear() - this.yearOfBirth;
+//     console.log(age);
+// }
+
+// var Athlete5 = function(name, yearOfBirth, job , olympicGames,medals){
+//     Person5.call(this,name,yearOfBirth,job);
+//     this.olympicGames=olympicGames;
+//     this.medals=medals;
+// }
+
+// Athlete5.prototype = Object.create(Person5.prototype);
+
+// Athlete5.prototype.wonMedal = function(){
+//     this.medals++;
+//     console.log(this.medals);
+// }
+
+// var johnAthlete5 = new Athlete5('john',1990,'swimmer',3,10);
+// johnAthlete5.calculateAge();
+// johnAthlete5.wonMedal();
+
+////ES6
+
+// class Person6 { //all classes must have a constructor
+//     constructor(name, yearOfBirth, job) {
+//         this.name = name;
+//         this.yearOfBirth = yearOfBirth;
+//         this.job = job;
+//     }
+//     calculateAge() {
+//         var age = new Date().getFullYear() - this.yearOfBirth;
+//         console.log(age);
+//     }
+
+//     static greeting(){
+//         console.log('Hey there');
+//     }
+// }
+
+// class Athlete6 extends Person6{
+//     constructor(name,yearOfBirth,job,olympicGames,medals){
+//         super(name,yearOfBirth,job);
+//         this.olympicGames-olympicGames;
+//         this.medals=medals;
+//     }
+
+//     wonMedal(){
+//         this.medals++;
+//         console.log(this.medals);
+//     }
+// }
+
+// const johnAthlete6 = new Athlete6('john',1990,'swimmer',3,10);
+// johnAthlete6.wonMedal();
+// johnAthlete6.calculateAge();
+
+////////////////////////////////////////////////
+// Coding challenge 8
+
+
+/*
+
+Suppose that you're working in a small town administration, and you're in charge of two town elements:
+1. Parks
+2. Streets
+
+It's a very small town, so right now there are only 3 parks and 4 streets. All parks and streets have a name and a build year.
+
+At an end-of-year meeting, your boss wants a final report with the following:
+1. Tree density of each park in the town (forumla: number of trees/park area)
+2. Average age of each town's park (forumla: sum of all ages/number of parks)
+3. The name of the park that has more than 1000 trees
+4. Total and average length of the town's streets
+5. Size classification of all streets: tiny/small/normal/big/huge. If the size is unknown, the default is normal
+
+All the report data should be printed to the console.
+
+HINT: Use some of the ES6 features: classes, subclasses, template strings, default parameters, maps, arrow functions, destructuring, etc.
+
+*/
+
+class Element {
+    constructor(name, bulidYear) {
+        this.name = name;
+        this.bulidYear = bulidYear;
+    }
+}
+
+class Park extends Element {
+    constructor(name, bulidYear, area, numTrees) {
+        super(name, bulidYear);
+        this.area = area; //km2 is unit here
+        this.numTrees = numTrees;
+    }
+
+    treeDensity() {
+        const density = this.numTrees / this.area;
+        console.log(`${this.name} has a tree density of ${density} trees per square km`);
+    }
+}
+
+class Street extends Element {
+    constructor(name, bulidYear, length, size = 3) {
+        super(name, bulidYear);
+        this.length = length;
+        this.size = size;
+    }
+
+    classifyStreet() {
+        const classification = new Map();
+        classification.set(1, 'tiny');
+        classification.set(2, 'small');
+        classification.set(3, 'normal');
+        classification.set(4, 'big');
+        classification.set(5, 'huge');
+        console.log(`${this.name} , build in ${this.bulidYear} is a ${classification.get(this.size)} street`);
+    }
+}
+
+//parks
+const allParks = [new Park('Green park', 1987, 0.2, 215),
+new Park('National Park', 1894, 2.9, 3541),
+new Park('Oak Park', 1953, 0.4, 949)];
+
+//street
+const allStreets = [new Street('Ocean Avenue',1999,1.1,4),
+new Street('Evergreen Street',2008,2.7,2),
+new Street('4th Street',2015,0.8),
+new Street('Sunset Boulevard',1982,2.5,5)];
+
+function calc(arr){
+
+    const sum = arr.reduce((prev,current,index)=> prev+current,0);
+
+    return [sum,sum / arr.length];
+}
+
+function reportParks(p){
+    
+    console.log('---------------------Parks Report---------------------');
+    //density
+    p.forEach(el=>el.treeDensity());
+    //average age
+    const ages = p.map(el=>2016-el.bulidYear);
+    const [totalAge, avgAge] = calc(ages);
+    console.log(`Our ${p.length} parks have an average age of ${avgAge} Years`);
+    //which parks has more that 1000 trees
+    const i = p.map(el=> el.numTrees).findIndex(el=>el>=1000) ;
+    console.log(`${p[i].name} has more than 1000 trees`);
+}
+
+function reportStreets(s){
+    console.log('---------------------Street Report---------------------');
+
+    // Total and average length of the town's streets
+
+    const [totalLength,avgLength] = calc(s.map(el=>el.length));
+    console.log(`Our ${s.length} Streets have a total length of ${totalLength}KM , with average length of ${avgLength}KM`);
+
+    // Classify sizes
+    s.forEach(el=>el.classifyStreet());
+}
+
+reportParks(allParks);
+reportStreets(allStreets);
